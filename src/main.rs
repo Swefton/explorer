5923d791e8468d6d7ec7ee29d4e6172fa7440894
Amrit <amrit.madhav.srivastava@gmail.com>
2025-06-29 12:45:04 -0400

    reading directories
    basic directory read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let path = "C:\\Users";
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        println!("{}", path.display());
+    }
+
+    Ok(())
 }
